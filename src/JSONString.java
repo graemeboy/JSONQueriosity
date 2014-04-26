d77d644d6b09798820ef8164939b1987961f712b
Vasilisa Bashlovkina <bashlovk@grinnell.edu>
2014-04-25 22:49:20 -0500

    number and string are perfect

--- src/JSONString.java
+++ src/JSONString.java
@@ -8,32 +8,69 @@
 public class JSONString
     extends JSONVal
 {
+  // +--------+----------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  /**
+   * The actual string
+   */
   String string;
 
-  // Constructors
+  // +--------------+----------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  /**
+   * Make a new JSONString
+   * 
+   * @param stringIn
+   */
   public JSONString(String stringIn)
   {
     this.string = stringIn;
   } // JSONString
 
+  // +---------+---------------------------------------------------------
+  // | Methods |
+  // +---------+
+  /**
+   *  The standard toString method
+   */
   public String toString()
   {
     return this.string;
-  }
+  }// toString()
 
+  /**
+   * Get the "value" of this string
+   * (necessary because the superclass has this method)
+   */
   public String get()
   {
     return this.string;
-  }
+  }// get()
 
+  /**
+   * Determine whether this is a string
+   * @return true
+   */
   public boolean isString()
   {
     return true;
-  } // type()
+  } // isString()
 
-  public int compareTo(String val)
+  /**
+   * Comapre this string to another
+   * @param <T>
+   * 
+   * @param val
+   * @return 0 is equal, < 0 is smaller, > 0 if greater 
+   */
+
+  public <T> int compareTo(T val)
   {
-    return (this.string.compareTo((String) val));
-  } // compareTo
-  
+    return (this.string.compareTo(val.toString()));
+  } // compareTo(String)
+
 }// JSONString class
